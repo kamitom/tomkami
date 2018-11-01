305d7dd55cbd0800ce775a0ef6b5b5a942dad8f8
Tom <tomha@T-HARA.local>
2018-11-01 10:10:16 +0800

    bug fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports.devided = function(a, b) {return a / b};
 
 module.exports.my_max = find_my_max;
 module.exports.my_min = my_min;
+module.exports.decimal_to_binary_test = DecimaltoBinaryTest;
 
 // find minimum
 function find_my_min( /*...*/ ) {
